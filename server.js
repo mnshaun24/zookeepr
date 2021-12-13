1a3075cd1bbe840118ad27f7c070013406607069
Shaun S <shaun.sticka@outlook.com>
2021-12-12 21:34:14 -0600

    added validation

--- server.js
+++ server.js
@@ -48,13 +48,29 @@ function createNewAnimal(body, animalsArray) {
     const animal = body;
     animalsArray.push(animal);
     fs.writeFileSync(
-        path.join(_dirname, "./data/animals.json"),
+        path.join(__dirname, "/data/animals.json"),
         JSON.stringify({ animals: animalsArray }, null, 2)
     );
 
     // return finished code to post route for response
     return animal;
 }
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
@@ -78,10 +94,14 @@ app.post("/api/animals", (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
 
-    // add animal to json file and animals array in this function
-    const animal = createNewAnimal(req.body, animals);
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+      res.status(400).send("The animal is not properly formatted.");
+    } else {
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+    }
 
-    res.json(req.body);
 });
 
 app.listen(PORT, () => {
