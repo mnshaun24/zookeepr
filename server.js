d49b6f2edc141b4b488845717b7061079244b714
Shaun S <shaun.sticka@outlook.com>
2021-12-14 21:17:07 -0600

    added some fetch and get requests for different html pages

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
+const { application } = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -95,6 +98,25 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
+// the point of this is to send an html to use for the page and is sent to the root route of the server
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
